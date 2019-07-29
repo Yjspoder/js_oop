40601190d3a15216297236e76b21345e80d38296
yjspoder <injose.joshi@gmail.com>
2019-07-29 23:57:49 +0530

    add changes

--- assignments/2/index.js
+++ assignments/2/index.js
@@ -18,7 +18,7 @@ var vicky = makePerson('Vicky', 24);
 
 // /********* Uncomment these lines to test your work! *********/
 console.log(vicky.name); // -> Logs 'Vicky'
-console.log(vicky.age); // -> Logs 24
+// console.log(vicky.age); // -> Logs 24
 
 
 
